9f434559c169a65981412a3600b617d55429569a
Andre-Pchela <124494848+Andre-Pchela@users.noreply.github.com>
2025-12-20 22:47:58 +0200

    Add files via upload

--- _online_shop/app/routes.py
+++ _online_shop/app/routes.py
@@ -5,20 +5,21 @@ bp=Blueprint('routes', __name__)
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    products = Product.query.all()
+    return render_template('index.html', products=products)    
 @bp.route('/products')
 def products():
     products = Product.query.all()
-    return render_template('product_list.html', products=products)
+    return render_template('index.html', products=products)
 
-@bp.route('/add', methods=['Get','Post'])
+@bp.route('/add', methods=['GET','POST'])
 def add_product():
-    if request.method =='Post':
+    if request.method =='POST':
         name=request.form['name']
         prise=request.form['prise']
         product=Product(name=name, prise=float(prise))
         db.session.add(product)
-        db.session.commit
-        flash("Product added")
-        return redirect(url_for('routes.product'))
+        db.session.commit()
+        #flash("Product added")
+        return redirect(url_for('routes.products'))
     return render_template('product_form.html',action='Add', product=None   )
