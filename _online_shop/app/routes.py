59a09b417358ae2af7ffe03dffeb1b5ea519e937
Andre-Pchela <124494848+Andre-Pchela@users.noreply.github.com>
2026-01-25 13:45:49 +0200

    Add files via upload

--- _online_shop/app/routes.py
+++ _online_shop/app/routes.py
@@ -29,3 +29,26 @@ def add_product():
         #flash("Product added")
         return redirect(url_for('routes.products'))
     return render_template('product_form.html',action='Add', product=None   )
+@bp.route('/delete/<int:product_id>', methods=['POST'])
+def delete_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    db.session.delete(product)
+    db.session.commit()
+    ##flash('Product deleted!')
+    return redirect(url_for('routes.products'))
+
+@bp.route('/update/<int:product_id>', methods=['GET','POST'])
+def update_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    if request.method=="POST":
+        product.name=request.form['name']
+        product.prise=float(request.form['prise'])
+        product.description=request.form['description']
+        product.stock=request.form['stock']        
+        product.is_active=bool(request.form.get('is_active'))
+        product.category=request.form['category']  
+        product.rating=request.form['rating']  
+        product.sale=bool(request.form.get('sale'))
+        db.session.commit()
+        return redirect(url_for('routes.products'))
+    return render_template('product_form.html',action='Update', product=product   )
